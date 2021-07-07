e08aed7bbbedb45b65f0e2d9a1218dcdb0e9def7
Micha Vegte, van der <mm.vdv@hotmail.com>
2021-07-07 15:27:13 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '13e79887f674366d79dca5944a8ff819'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'be328b54-7b7d-4cff-8d2e-c39088926f1d'
+};
